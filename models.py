93f2620152c7bfd310778bef0da17233f6c348ba
danil_proskurin <danielpashtetov@gmail.com>
2025-03-06 15:37:34 +0700

    update db models, add docker-compose with services and network

--- models.py
+++ models.py
@@ -7,19 +7,19 @@ Base = declarative_base()
 
 class OrderModel(Base):
     __tablename__ = 'order'
-    user_id = Column(Integer, primary_key=True, index=True)
-    items = relationship("OrderItemModel", back_populates="order")
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, nullable=False)
     total_price = Column(Float, nullable=False)
     status = Column(String(50), default="created")
+    order_items = relationship("OrderItemModel", back_populates="order")
 
 
 class ItemModel(Base):
     __tablename__ = 'item'
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
+    name = Column(String(100), nullable=False)
     description = Column(String(255), nullable=True)
     price = Column(Float, nullable=False)
-
     order_items = relationship("OrderItemModel", back_populates="item")
 
 
@@ -28,6 +28,5 @@ class OrderItemModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     order_id = Column(Integer, ForeignKey('order.id'), nullable=False)
     item_id = Column(Integer, ForeignKey('item.id'), nullable=False)
-
     order = relationship("OrderModel", back_populates="order_items")
     item = relationship("ItemModel", back_populates="order_items")
