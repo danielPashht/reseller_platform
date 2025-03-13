bf6676eb6f3af929af211cd1ee3c3c2964df079d
danil_proskurin <danielpashtetov@gmail.com>
2025-03-13 20:37:51 +0700

    test: refactor status code assertions and add new test cases for order retrieval

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-import pytest
 from core.config import ADMIN_SECRET
 from fastapi.testclient import TestClient
 from core.main import app
@@ -8,9 +7,14 @@ client = TestClient(app)
 client.headers.update({"x-api-key": ADMIN_SECRET})
 
 
+def check_status_code(response, expected_status_code):
+    assert response.status_code == expected_status_code, \
+        f"Incorrect status code: {response.status_code}, expected {expected_status_code}"
+
+
 def test_read_items():
     response = client.get("/items/")
-    assert response.status_code == 200
+    check_status_code(response, 200)
     assert isinstance(response.json(), list)
 
 
@@ -21,5 +25,18 @@ def test_create_order():
         "total_price": 100.0
     }
     response = client.post("/order/", json=order_data)
-    assert response.status_code == 201
+    check_status_code(response, 201)
     assert "order_id" in response.json()
+    assert isinstance(response.json()["order_id"], int)
+
+
+def test_read_orders():
+    response = client.get("/orders/")
+    check_status_code(response, 200)
+    assert isinstance(response.json(), list)
+
+
+def test_forbidden_access_to_api():
+    client.headers.clear()
+    response = client.get("/orders/")
+    check_status_code(response, 403)
